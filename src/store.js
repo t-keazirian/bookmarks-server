1b25dd021dcba8c2ebfcd2e9fb93f37d74e3e63c
Taylor Keazirian <taylork26@gmail.com>
2021-03-02 15:21:06 -0600

    router implementation

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+	{
+		id: 1,
+		title: 'bookmark test 1',
+		url: 'www.google.com',
+		rating: 1,
+		description: 'description for bookmark test 1',
+	},
+];
+
+module.exports = { bookmarks };
